e150a8a28547040ee1661239f5490ae9f6d0d22f
Jose Rodriguez <josenect@hotmail.es>
2021-01-25 18:30:18 -0500

    re realizo tipos de valores en JavaScript

---
+++ valores.js
@@ -0,0 +1,32 @@
+// tipos Primitivos
+40
+true 
+false
+null
+undefined
+
+
+//tipos de Objectos
+//Array
+let frutas = ["Manzana", "Banana"]
+
+
+//objecto simple con sus propiedades
+  const o = {
+    name: "Manz",
+    life: 99,
+  };
+
+
+  const obj = {
+    name: "Manz",
+    life: 99,
+    saludar: function () {
+      return "Hola!";
+    },
+  };
+  
+
+
+  //un formato de texto tipo Json
+  const json = '{ "name": "Manz", "life": 99 }';
