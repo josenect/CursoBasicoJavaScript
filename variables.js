b52b4279b9eea658bed5b7e9964e4d0a86cdf732
Jose Rodriguez <josenect@hotmail.es>
2021-01-25 19:46:56 -0500

    Variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+//variables
+
+var nombre = "oscar" ;
+
+//declarar 
+
+var edad ; 
+
+
+//inicializar
+edad = 30;
+
+
+//array
+var elementos = ["computador","celular" ,10];
+
+
+//Objectos
+
+var persona ={
+    nombre:"jose",
+    edad:24
+
+}
